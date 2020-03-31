24065cc3851d3021210b2b112d83fdbabf82607b
jeng <jamespaulespea@gmail.com>
2020-03-31 22:48:30 +0800

    fix footer, API, simplified

--- js/main.js
+++ js/main.js
@@ -1,9 +1,95 @@
 
-
-
-
-
 function selek() {
+
+    var balyu = document.getElementById("format").value;
+
+    // Constructor
+
+    const konvert = new Intl.NumberFormat('en');    
+    const persent = new Intl.NumberFormat('en',{style:'percent'});
+
+   
+
+    fetch('https://covid19.mathdro.id/api/countries/' + balyu)
+        .then(response => response.json())
+        .then(data => {
+
+
+
+             // paNG FETCH NG LAMAN NG SELECT BOX
+
+             var sagot = document.getElementById("format");
+             var sagotuli= sagot.options[sagot.selectedIndex].text;
+             var e = sagotuli.fontcolor('#f5f5f5');
+             var eee = document.getElementById("name");  
+             eee.innerHTML = ('Country Name:' +  '&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp' + e);  // result sa name
+ 
+ 
+
+          
+            var a = data.lastUpdate;   //date  ito ok 
+            var aa = moment(a).format('LLLL');;
+            var b = document.getElementById("date");  
+            
+   
+            b.innerHTML = ('Last Updated: ' + '&nbsp &nbsp'  + aa);  // result sa name
+
+
+
+            var bb = data.confirmed.value;   //  para confirm
+            var bb1 = konvert.format(bb);
+            var bcolor = bb1.fontcolor('#42A5F5');  
+            var bbb = document.getElementById("confirm");  
+            
+   
+            bbb.innerHTML = ('Total no. of confirmed: ' +  '&nbsp &nbsp &nbsp &nbsp ' + bcolor);  // result sa name
+
+
+
+            var cc = data.recovered.value;   //  para recovered
+            var cc1 = konvert.format(cc);
+            var ccolor = cc1.fontcolor('#27fa00');  
+            var ccc = document.getElementById("recovered");  
+            
+   
+            ccc.innerHTML = ('Total no. of recoveries: ' +  '&nbsp &nbsp &nbsp &nbsp &nbsp' + ccolor);  // result sa name
+       
+
+
+
+            var dd = data.deaths.value;   //  para d
+            var dd1 = konvert.format(dd);
+            var dcolor = dd1.fontcolor('#f70d1a');
+            var ddd = document.getElementById("d");  
+            
+   
+            ddd.innerHTML = ('Total no. of deaths: ' +  '&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp' + dcolor);
+       
+            
+           
+
+            
+
+        
+
+
+         
+         
+
+
+            
+
+         
+
+    }
+    
+
+    )
+}
+
+
+
+function selek1() {
 
 
 
